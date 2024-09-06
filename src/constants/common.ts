2173df2fa400e4809baffd4d6fc7067f6f4d0417
LeTravs <munk949@gmail.com>
2024-09-06 08:10:03 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,19 @@
+export const NAME = 'Lee C. Traveros';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Im a freelance digital product creator passionate about creating interactive digital storybooks that spark curiosity and ignite a love for reading in young minds. I believe that stories have the power to transport us to new worlds, teach us valuable lessons, and inspire us to dream big. My goal is to create digital storybooks that not only entertain but also empower children to explore, learn, and grow.';
+
+export const ABOUT_TEXT_2 =
+  'Specializing in editing and creating digital products for children, I bring stories, stickers, coloring pages, and more to life. My passion lies in crafting engaging experiences that spark imagination and creativity. Im dedicated to delivering high-quality digital products that entertain, educate, and delight both children and parents';
+
+export const ABOUT_TEXT_3 =
+  'Edit photos and videos to enhance visual appeal, correct errors, and create engaging content. Design and create digital storybooks, stickers, coloring books, and other interactive products.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  "I love bringing stories, stickers, and coloring pages to life.  It's so rewarding to see the joy on a child's face when they discover something new and creative. My goal is to create high-quality digital products that spark imagination, entertain, and delight, especially for kids.";
+
+export const RESUME_TEXT_2 =
+   "Digital Product Creator with over 1 year of experience in crafting and enhancing storybooks for children. I use Canva to design and create these storybooks, selling them online to provide fun, educational, and engaging experiences for kids.";
+
